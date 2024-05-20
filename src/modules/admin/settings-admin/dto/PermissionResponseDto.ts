6749c0b1694fc6231bed3d8e1a591fa494b08041
Guilherme Artigas <guilherme.artigas92@gmail.com>
2024-05-20 10:52:42 -0300

    feat: response dto de todas as permissoes

---
+++ src/modules/admin/settings-admin/dto/PermissionResponseDto.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class PermissionResponseDto {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
